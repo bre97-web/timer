82e1361cf3db3340ff3381f2e5a1a5ceb387192a
bre 97 <by.sgatn3@Gmail.com>
2023-09-08 16:04:32 +0800

    fix the css in timer button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative max-h-screen min-h-screen flex flex-col h-full">
         <Header></Header>
-        <main class="z-[0] container self-center m-4 flex-grow overflow-y-auto rounded-3xl">
+        <main class="container self-center m-4 flex-grow overflow-y-auto rounded-3xl">
             <div class="h-[3000px]">13</div>
             <CreateTimerButton></CreateTimerButton>
         </main>
--- src/components/fixed-button/CreateTimerButton.vue
+++ src/components/fixed-button/CreateTimerButton.vue
@@ -2,14 +2,14 @@
     <FixedLayout class="right-4 bottom-4">
         <ExpanedLayout class="flex gap-2 flex-col-reverse">
             <template v-slot:action="{ setIsExpanded, isExpanded }">
-                <md-fab @click="setIsExpanded(true)" :disabled="isExpanded" label="Starts" class="z-[1]">
+                <md-fab @click="setIsExpanded(true)" :disabled="isExpanded" label="Starts">
                     <md-icon slot="icon">add</md-icon>
                 </md-fab>
             </template>
 
             <template v-slot:expanded-content="{ setIsExpanded, isExpanded }">
                 <FlexLayout class="gap-2 flex-col justify-center items-center transition-all"
-                    :class="isExpanded ? ' opacity-100 flex z-[1]' : ' opacity-0 -z-[1]'" @click="setIsExpanded(false)">
+                    :class="isExpanded ? ' opacity-100 flex z-[1]' : ' opacity-0'" @click="setIsExpanded(false)">
                     <md-fab lowered label="Timer" class="disabled-force">
                         <md-icon slot="icon">timer</md-icon>
                     </md-fab>
@@ -17,7 +17,7 @@
                         <md-icon slot="icon">alarm</md-icon>
                     </md-fab>
                     <div
-                        class="hasMask fixed top-0 left-0 -z-[1] bg-[--md-sys-color-inverse-on-surface-light] opacity-75 w-full h-full"
+                        class="-z-[1] hasMask fixed top-0 left-0 bg-[--md-sys-color-inverse-on-surface-light] opacity-75 w-full h-full"
                         :class="isExpanded ? 'pointer-events-auto' : 'pointer-events-none'"
                     ></div>
                 </FlexLayout>
