ac6dfdf29b12cb146ca8a71645822a9ca61d93be
bre 97 <by.sgatn3@Gmail.com>
2023-09-09 15:03:13 +0800

    remove the variable targetTimer

--- src/components/timer/edit-label/editLabel.ts
+++ src/components/timer/edit-label/editLabel.ts
@@ -1,19 +1,14 @@
 import { MDButton, MDDialog, MDTextField } from "@/types/MDComponents"
 import { EventPackage } from "@/store/TimerStore"
-import { ref } from "vue"
 
-const targetTimer = ref<EventPackage | null>()
-
-export const willEditLabel = (e: EventPackage) => {
-    targetTimer.value = e
-
+export const willEditLabel = (target: EventPackage) => {
     const dialog = openEditLabelDialog()
     
     dialog.addEventListener('submit', (e) => {
         const value = ((e.target as HTMLElement).children.item(0) as MDTextField).value
 
         if((e.submitter as MDButton).value === 'ok') {
-            targetTimer.value.label = value
+            target.label = value
         }
     })
 
