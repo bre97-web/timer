1d0a738e3085727cec8f58598f7ff0e392b76222
bre 97 <by.sgatn3@Gmail.com>
2023-09-10 10:52:43 +0800

    fix css

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="background relative max-h-screen min-h-screen flex flex-col h-full w-full">
+    <div class="background relative max-h-screen flex flex-col h-full w-full">
         <Header></Header>
         <main class="relative surface self-center flex-grow overflow-y-auto rounded-3xl w-full">
             <router-view></router-view>
             <CreateButton></CreateButton>
             <EditLabelDialogVue></EditLabelDialogVue>
         </main>
-        <nav></nav>
+        <nav class="right-0 bottom-0 w-full overflow-clip h-fit flex-none bg-blue-400">
+            <BottomNavigation></BottomNavigation>
+        </nav>
     </div>
 </template>
 
@@ -14,6 +16,7 @@
 import CreateButton from '@/components/timer/create-timer/CreateButton.vue'
 import EditLabelDialogVue from '@/components/timer/edit-label/EditLabelDialog.vue'
 import Header from '@/components/header/Header.vue'
+import BottomNavigation from '@/components/navigate/BottomNavigation.vue'
 import { onMounted } from 'vue';
 import { initDarkToHtmlClass } from './store/DarkStore';
 import { useTimerStore } from './store/TimerStore';
---
+++ src/components/navigate/BottomNavigation.vue
@@ -0,0 +1,20 @@
+<template>
+    <md-navigation-bar ref="bottomNavigationBarRef">
+        <md-navigation-tab v-for="e in routerList" :key="e.path" :label="e.label"></md-navigation-tab>
+    </md-navigation-bar>
+</template>
+
+<script setup lang="ts">
+import { getNavigationList } from '@/scripts/navigate'
+import { ref } from 'vue'
+
+const routerList = getNavigationList()
+
+const bottomNavigationBarRef = ref()
+
+
+</script>
+
+<style scoped>
+
+</style>
